3251f8ff73ba12f26783dbc066761c63758cf2eb
=domagojleskovic <domagoj.leskovic24@gmail.com>
2023-12-23 21:02:55 +0100

    Inital commit

---
+++ app/src/main/java/com/domagojleskovic/constantio/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.domagojleskovic.constantio
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun LoginScreen(
+    @DrawableRes img : Int
+) {
+    
+}
