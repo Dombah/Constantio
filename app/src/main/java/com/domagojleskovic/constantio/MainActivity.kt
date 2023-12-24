3af964bf671ccfe547c1bf90bc08add76b01e7fd
=domagojleskovic <domagoj.leskovic24@gmail.com>
2023-12-24 14:56:00 +0100

    Add template UI to the Log In screen

--- app/src/main/java/com/domagojleskovic/constantio/MainActivity.kt
+++ app/src/main/java/com/domagojleskovic/constantio/MainActivity.kt
@@ -3,14 +3,20 @@ package com.domagojleskovic.constantio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.domagojleskovic.constantio.ui.theme.ConstantioTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +28,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    LoginScreen()
                 }
             }
         }
     }
 }
-
 
--- app/src/main/java/com/domagojleskovic/constantio/ui/theme/Color.kt
+++ app/src/main/java/com/domagojleskovic/constantio/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkBlue_Palette = Color(0xFF2B2E4A)
+val Red_Palette = Color(0xFFE84545)
+val LightRed_Palette = Color(0xFF903749)
+val Brownish_Palette = Color(0xFF53354A)
+
+/*
+  <color name= "black_Palette">#222831</color>
+  <color name= "darkGrey_Palette">#393E46</color>
+  <color name= "teal_Palette">#00ADB5</color>
+  <color name= "white_Palette">#EEEEEE</color>
+*/
