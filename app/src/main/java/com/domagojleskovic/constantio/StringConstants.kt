bf8db1e1dc8b3826fe98295c04c1965a31c9a8da
=domagojleskovic <domagoj.leskovic24@gmail.com>
2024-01-24 20:06:56 +0100

    Add helper function to house string paths

---
+++ app/src/main/java/com/domagojleskovic/constantio/StringConstants.kt
@@ -0,0 +1,8 @@
+package com.domagojleskovic.constantio
+
+class StringConstants {
+    companion object FirebasePaths {
+        const val firebaseUserProfilePicturePath = "UserProfilePictures/"
+        const val firebaseUserPicturesPath = "UserPictures/"
+    }
+}
